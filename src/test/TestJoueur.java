ab05e24d3ae49dfa520326bc323e4e2681dabb2f
Razi Askri <askrirazi@gmail.com>
2021-12-20 22:30:35 +0100

    add tests class joueur

--- src/test/TestJoueur.java
+++ src/test/TestJoueur.java
@@ -7,7 +7,7 @@ public class TestJoueur {
 	public static void main(String[] args){
 		TestJoueur testJoueur = new TestJoueur();
 		//testJoueur.test1();
-		testJoueur.test2();	
+		//testJoueur.test2();	
 		//testJoueur.test3();
 		//testJoueur.test4();
 		//testJoueur.test5();	
