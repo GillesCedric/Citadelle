9ce29e1dc14ed4853ed281f08db8d3ff7be12fe9
Razi Askri <askrirazi@gmail.com>
2021-12-20 21:58:12 +0100

    add class architechte and condottiere

---
+++ src/modele/Architecte.java
@@ -0,0 +1,16 @@
+package modele;
+
+public class Architecte extends Personnage {
+	
+	public Architecte(String nom, int rang, String caracteristiques) {
+		super(nom, rang, caracteristiques);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public void utiliserPouvoir() {
+		//il reste a ajouter la fonction des cartes quartiers 
+		//dans la classe personnage		
+	}
+
+}
---
+++ src/modele/Condottiere.java
@@ -0,0 +1,19 @@
+package modele;
+
+public class Condottiere extends Personnage {
+
+	public Condottiere(String nom, int rang, String caracteristiques) {
+		super(nom, rang, caracteristiques);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void percevoirRessourcesSpecifiques() {
+		super.percevoirRessourcesSpecifiques();
+	}
+	
+	@Override
+	public void utiliserPouvoir() {
+		// TODO Auto-generated method stub
+	}
+
+}
