5c951c24071923b3f466814c7563d68c5ebc8392
AlSahim1738 <94036149+AlSahim1738@users.noreply.github.com>
2022-01-06 11:30:11 +0100

    Update and rename Sorciere to Sorciere.java

---
+++ src/modele/Sorciere.java
@@ -0,0 +1,14 @@
+class Sorciere() extends Personnage
+{
+       public Sorciere() 
+       {
+              super( Nom.Sorciere, Rang.Sorciere, Caracteristiques.Sorciere);
+       }
+       public utiliserPouvoir()
+       {
+              //Annoncer le personnage à ensorceler
+                     System.out.println(" Quel personnage voulez vous ensorceler ?");
+                     //Afficher la liste des personnages 
+                     for (i=0 ; i < pesonnae
+       }
+}
