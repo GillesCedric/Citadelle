5d299887cd6b9f1906cd67a3d0e06d1d61383340
AlSahim1738 <94036149+AlSahim1738@users.noreply.github.com>
2022-01-15 19:33:42 +0100

    Update Alchimiste.java

--- src/modele/Alchimiste.java
+++ src/modele/Alchimiste.java
@@ -27,27 +27,12 @@ public class Alchimiste extends Personnage {
 	public void utiliserPouvoir() 
 	{
 		// TODO Auto-generated method stub
-		 if(!getAssassine()){
-	            
-	        }else if(getJoueur().equals(null)){
-	            System.out.println("Ce personnage n'est pas attribué");
-	        }else{
-	            System.out.println("Vous avez été assassiné, vous ne pouvez pas utiliser votre pouvoir");
-	        }
-
 	}
 
 	@Override
 	public void utiliserPouvoirAvatar() 
 	{
-		// TODO Auto-generated method stub
-		 if(!getAssassine()){
-	            
-	        }else if(getJoueur().equals(null)){
-	            System.out.println("Ce personnage n'a pas été attribué");
-	        }else{
-	            System.out.println("Vous avez été assassiné, vous ne pouvez pas utiliser votre pouvoir");
-	        }
+		
 
 	}
 	  @Override
