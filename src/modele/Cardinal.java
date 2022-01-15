71ddb3e0197f8b15921e0c860d1a5ff34b72e69f
AlSahim1738 <94036149+AlSahim1738@users.noreply.github.com>
2022-01-15 19:24:20 +0100

    Update Cardinal.java

--- src/modele/Cardinal.java
+++ src/modele/Cardinal.java
@@ -30,19 +30,50 @@ public class Cardinal extends Personnage {
 		
 
 	}
-	public void percevoirRessourcesSpecifiques()
-	{
-		int i;
-		Quartier cit[]= getJoueur().getCite();
-		for(i=0; i< getJoueur().nbQuartiersDansCite();i++)
-		{
-			
-			if(cit[i].getType().equals(Quartier.TYPE_QUARTIERS[0]))
-			{
-				getJoueur().ajouterPieces(1);
-			}
-		}
-	}
+	 @Override
+    public void percevoirRessourcesSpecifiques(){
+        if(!getAssassine()){
+                Quartier[] cite = getJoueur().getCite();
+                int count=0;
+                for(int i=0; i < cite.length;i++){
+                    if(cite[i] != null && cite[i].getType().equals("RELIGIEUX")){
+                        count++;
+                    }
+                    if(cite[i] != null && cite[i].getNom().equals("Ecole de Magie")){
+						count +=1;
+                        System.out.println("L'école de magie vous rapprote 1 piece de plus");
+
+					}
+                }
+                if(getPlateau().getPioche().nombreElements()>=count) {
+                	for(int i = 0; i<count; i++) {
+                		getJoueur().ajouterQuartierDansMain(getPlateau().getPioche().piocher());
+                	}
+                	if(getJoueur().isSimule()==true) {
+                		System.out.println("     Vous avez recu "+count+" quartier(s) grace � votre cit�");
+                	}
+                	else {
+                		System.out.println("Vous avez recu "+count+" quartier(s) gr�ce � votre cit�");
+                	}
+                }
+                else {
+                	int max = getPlateau().getPioche().nombreElements();
+                	for(int i=0; i<max; i++) {
+                		getJoueur().ajouterQuartierDansMain(getPlateau().getPioche().piocher());
+                	}
+                	if(getJoueur().isSimule()==true) {
+                		System.out.println("     Vous avez recu  "+max+" quartier au lieu de "+count+"  la pioche n'a pas assez de carte");
+                	}
+                	else {
+                		System.out.println("Vous n'avez re�u  "+max+" quartier  au lieu de "+count+"  la pioche n'a pas assez de carte");
+                	}
+                }
+            }else if(getJoueur().equals(null)){
+                System.out.println("Ce personnage n'a pas été attribué");
+            }else{
+                System.out.println("Vous avez été assassiné, vous ne pouvez pas utiliser votre pouvoir");
+        }
+    }
 	@Override
 	public void utiliserPouvoirAvatar() {
 		// TODO Auto-generated method stub
