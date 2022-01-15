b189d1c29b52c76fc5a801c121d7991ad36f9d9a
AlSahim1738 <94036149+AlSahim1738@users.noreply.github.com>
2022-01-15 19:32:57 +0100

    Update Negociant.java

--- src/modele/Negociant.java
+++ src/modele/Negociant.java
@@ -34,5 +34,28 @@ public class Negociant extends Personnage {
 		// TODO Auto-generated method stub
 
 	}
+	@Override
+	 public void percevoirRessourcesSpecifiques()
+	 {
+    		if(!getAssassine())
+    		{ 
+        	System.out.println("    Perception des ressources spécifiques");
+        	Quartier[] cite = getJoueur().getCite();
+            	int count=0;
+            for(int i=0; i < cite.length;i++){
+                if(cite[i] != null && cite[i].getType().equals("COMMERCANT")){
+                    count++;
+                }
+                if(cite[i] != null && cite[i].getNom().equals("Ecole de Magie")){
+                    count +=1;
+                    System.out.println("L'école de magie vous rapprote 1 piece de plus");
+
+                }
+            }
+
+            getJoueur().ajouterPieces(count);
+	 
+	}
+    }
 
 }
