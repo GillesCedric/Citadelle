c6514056a9346ac47bc103b50e6141b6f69caf68
AlSahim1738 <94036149+AlSahim1738@users.noreply.github.com>
2022-01-16 23:15:09 +0100

    Update Empereur.java

--- src/modele/Empereur.java
+++ src/modele/Empereur.java
@@ -22,7 +22,27 @@ public class Empereur extends Personnage {
 		super(Nom.EMPEREUR, Rang.EMPEREUR, Caracteristiques.EMPEREUR);
 		// TODO Auto-generated constructor stub
 	}
-
+	public void percevoirRessourcesSpecifiques()
+	{
+		int piece = 0;
+		if (super.getJoueur() != null) 
+		{
+			Quartier[] cite = super.getJoueur().getCite();
+			for (int i = 0; i<cite.length ; i++) 
+			{
+				if (cite[i] != null && cite[i].getType().equals(TYPE_QUARTIERS[2]))
+				{
+					piece += 1;
+				}
+				if(cite[i] != null && cite[i].getNom().equals("Ecole de Magie"))
+				{
+					piece +=1;
+				}
+			}
+			System.out.println(" Vous gagnez " +piece +" en plus grace à vos quartier nobles et à la merveille Ecole de magie");
+			super.getJoueur().ajouterPieces(piece);
+		}
+	}
 	@Override
 	public void utiliserPouvoir() {
 		// TODO Auto-generated method stub
