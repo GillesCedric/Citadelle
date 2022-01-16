6e59d38a87b570e787967df538e68603fb46a96c
AlSahim1738 <94036149+AlSahim1738@users.noreply.github.com>
2022-01-16 23:20:56 +0100

    Update Marchande.java

--- src/modele/Marchande.java
+++ src/modele/Marchande.java
@@ -13,6 +13,7 @@ public class Marchande extends Personnage
 	public void percevoirRessourcesSpecifiques()
 	{
 		int i;
+		int piece = 0;
 		Quartier cit[]= getJoueur().getCite();
 		for(i=0; i< getJoueur().nbQuartiersDansCite();i++)
 		{
@@ -20,12 +21,20 @@ public class Marchande extends Personnage
 			if(cit[i].getType().equals(Quartier.TYPE_QUARTIERS[3]))
 			{
 				getJoueur().ajouterPieces(1);
+				piece++;
+			}
+			if(cit[i].getNom().equals("Ecole de Magie"))
+			{
+				getJoueur().ajouterPieces(1);
+				piece++;
 			}
 		}
+		System.out.println("Grace à vos quartier religieux et/ou la merveille Ecole de magie vous gagnez : " +piece +"pieces" );
 	}
 	public void utiliserPouvoir()
 	{
 		getJoueur().ajouterPieces(1);
+		System.out.println("Grace à votre pouvoir vous gagnez une piece d'or");
 	}
 	@Override
 	public void utiliserPouvoirAvatar() {
